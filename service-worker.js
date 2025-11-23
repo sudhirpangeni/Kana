72a80411ff9f3ef4882bf7a4b1334d8c9afe5bab
sudhirpangeni <sudhirpangeni20@gmail.com>
2025-11-24 00:47:40 +0545

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -7,17 +7,6 @@ const urlsToCache = [
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null)))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
