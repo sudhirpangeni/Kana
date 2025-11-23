47debf9513e092bfba4345dc519bb1d7092e0236
sudhirpangeni <sudhirpangeni20@gmail.com>
2025-11-23 21:53:48 +0545

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("kana-cache").then(cache => {
+      return cache.addAll(["index.html","manifest.json","icon.png"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
